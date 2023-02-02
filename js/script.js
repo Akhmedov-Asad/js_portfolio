a2872246511e9753fc55429a9ffdc0359f61b5d5
Akhmedov-Asad <akhmedovasadbek76@gmail.com>
2023-02-02 17:19:00 +0500

    js__portfolio

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+
+// const calcAverageResult = (attempt1, attempt2, attempt3) => {
+//     const averageResult = (attempt1 + attempt2 + attempt3) / 3;
+//     return averageResult;
+// }
+// const team1 = calcAverageResult(90, 80, 170);
+// const team2 = calcAverageResult(90, 88, 160);
+
+// const winnerFunc = function (data1, data2) {
+//     if (data1 > data2) {
+//         return ('winner team1');
+//     } else if (data1 === data2) {
+//         return 'durrang';
+//     } else {
+//         return 'winner team2';
+//     }
+// }
+// console.log(winnerFunc(team1, team2));
+
+
+// const totalStudents = 2500;
+// const calcPersonage = function(studentsNumber){
+//    const calc = studentsNumber / totalStudents * 100;
+//    return `Sizning guruhdagio'quvchilar markazning umumiy o'quvchilarini ${calc.toFixed(1)}% ni tashkil qilar ekan`
+// }
+// console.log(calcPersonage(22));
+
+
+// function myFun(a = 1, b = 1, c = 1) {
+//      return a + b + c;
+// }
+// console.log(myFun(2));
+
